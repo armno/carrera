51c33719844baf5a6defd5498b116c9a4655db33
ArmNo <arm@snapplabs.com>
2013-05-15 22:57:51 +0700

    added some more spec

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -7,5 +7,6 @@ describe Blog do
 		it "should have a link to create new post"
 		it "should display a post"
 		it "should add new post"
+		it "should display error page if the post is not found"
 	end
 end
